c61ec46a0d528f1019eca7104b8ae3dcd8f0c23d
bishwasregmi <bishwas182@gmail.com>
2020-12-01 18:47:49 +0100

    q2 working

--- q2.py
+++ q2.py
@@ -4,30 +4,37 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import scipy
-from scipy.stats import t
+from scipy.stats import t, norm
 import matplotlib.pyplot as plt
 
 fileObject = open("question2_transport_time.txt", "r")
-data =fileObject.read().split()
-data =list(map(float,data))
-print(data)
+# fileObject = open("transport_time_64Kbyte.txt", "r")
+data = fileObject.read().split()
+data = list(map(float, data))
 
-confidence_level = 0.95
-degrees_freedom =len(data)-1
-sample_mean = np.mean(data)
-sample_standard_error = scipy.stats.sem(data)
+alpha_1 = 0.98
+alpha_2 = 0.80
+dof = len(data) - 1
+mu = np.mean(data)
+std = np.std(data)
+sem = std / np.sqrt(len(data))
 
+t1 = abs(t.ppf((1 - alpha_1) / 2, df=dof)) * sem
+t2 = abs(t.ppf((1 - alpha_2) / 2, df=dof)) * sem
 
-confidence_interval = t.interval(confidence_level, degrees_freedom, sample_mean, sample_standard_error)
-tplot = t.pdf(data,degrees_freedom)
-print(sample_mean)
-print(sample_standard_error)
-print(confidence_interval)
-plt.plot(data)
-plt.plot(tplot)
+c1 = [mu - t1, mu + t1]
+c2 = [mu - t2, mu + t2]
+
+# plt.hist(data, density=True, bins=20, color='y')
+plt.axvline(mu, color='k', label="sample_mean")
+plt.axvline(c1[0], color='g', label="ConfidenceInterval_"+str(alpha_1))
+plt.axvline(c1[1], color='g')
+plt.axvline(c2[0], color='r', label="ConfidenceInterval_"+str(alpha_2))
+plt.axvline(c2[1], color='r')
+
+xmin, xmax = plt.xlim()
+x = np.linspace(0.00085, 0.00105, 1000)
+t_plot = t.pdf(x, dof, mu, sem)
+plt.plot(x, t_plot)
+plt.legend()
 plt.show()
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
