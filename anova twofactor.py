66bb361d7ef4f046545d7992a96595e63a451d05
bishwasregmi <bishwas182@gmail.com>
2020-12-07 23:21:39 +0100

    calculating CI for dds

--- anova twofactor.py
+++ anova twofactor.py
@@ -6,6 +6,7 @@ from statsmodels.stats.anova import anova_lm
 import seaborn as sns
 import researchpy as rp
 import statsmodels.api as sm
+from scipy.stats import t
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 from statsmodels.graphics.factorplots import interaction_plot
 import matplotlib.pyplot as plt
@@ -24,8 +25,9 @@ df = pd.DataFrame({'latency' : latency,
                    'data_size' : np.repeat(['256B', '128kB'], 1500),
                    'dds' : np.r_[np.repeat(['fastrtps', 'opensplice', 'connext'],500),
                                  np.repeat(['fastrtps', 'opensplice', 'connext'],500)]})
-sns.boxplot(x = 'dds', y = 'latency', hue= 'data_size', data = df, showfliers=False)
-plt.show()
+# sns.boxplot(x = 'dds', y = 'latency', hue= 'data_size', data = df, showfliers=False)
+# plt.grid()
+# plt.show()
 print(f"DataFrame summary:\n{rp.summary_cont(df.groupby(['data_size', 'dds']))['latency']}")
 # print(df)
 
@@ -49,4 +51,35 @@ print(f"Anova table:\n {res}")
 # print(mc_results)
 
 
-latency = np.array(load_meas_data_q4())
+# ------------CALCULATING CONFIDENCE INTERVALS FOR DDSes-----------
+
+
+latency = load_meas_data_q4()
+fastrtps = np.concatenate((latency[0][0], latency[1][0]))
+opensplice = np.concatenate((latency[0][1], latency[1][1]))
+connext = np.concatenate((latency[0][2], latency[1][2]))
+
+dds_meas = [fastrtps, opensplice, connext]
+
+dds_mean = np.mean(dds_meas)
+
+ssr = res['sum_sq'][3]                  # error variance(ssr) taken from the anova table
+
+dds_std = ssr*np.sqrt(((3-1)/(3*2*500)))   # s_alpha = ssr * sqrt((a-1)/abr)
+
+
+
+for i,data in enumerate(dds_meas):
+    alpha = 0.95
+    dof = len(data) - 1
+    mu = np.mean(data)
+    std = np.std(data)
+    sem = std / np.sqrt(len(data))
+
+    t_val = abs(t.ppf((1 - alpha) / 2, df=dof))
+
+    c = [mu - t_val * sem, mu + t_val * sem]
+
+    print(f"Confidence interval {i} = {c}")
+
+
