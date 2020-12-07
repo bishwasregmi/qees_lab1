c8b7f20a69733156fb96f0b1f360721814379647
NishuVid <nishuvidyarthi1012@gmail.com>
2020-12-07 12:16:07 +0100

    added new files

--- anova twofactor.py
+++ anova twofactor.py
@@ -1,8 +1,13 @@
 # load packages
 import pandas as pd
-import seaborn as sns
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm
+from statsmodels.graphics.factorplots import interaction_plot
+import matplotlib.pyplot as plt
+from scipy import stats
 # load data file
-d = pd.read_csv("measurements/", sep="\t")
+data = pd.read("measurements/", sep="\t")
+
 
 
 d_melt = pd.melt(d, id_vars=['data'], value_vars=['fastrtps', 'opensplice', 'connext'])
